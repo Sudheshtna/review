fb9fb67febd59f78d41792381d75dfb856e07683
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-09-02 09:18:44 +0530

    added: review

---
+++ review.sh
@@ -0,0 +1,16 @@
+read -p "enter the number:" number
+if((number == 1))
+then
+echo " one"
+elif((number == 2))
+then
+echo "two"
+elif((number == 3))
+then
+echo "three"
+elif((number == 4))
+then
+echo " four "
+else
+echo " no digit"
+fi
